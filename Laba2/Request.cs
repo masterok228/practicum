a480eda8f663658099318100e426305865fe61fa
masterok228 <49608887+masterok228@users.noreply.github.com>
2020-04-22 15:38:52 +0500

    add task2

---
+++ Laba2/Request.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Laba2
+{
+    public partial class Request
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+
+        public Request() => ServiceRequest = new HashSet<ServiceRequest>();
+
+        public virtual Сlient Сlient { get; set; }
+        public virtual ICollection<ServiceRequest> ServiceRequest { get; set; }
+    }
+}
